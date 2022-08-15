3b09c52a6321ee3c1e3c2da3ed476d0d4ece3299
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-08-14 21:51:25 -0300

    Pushing 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,6 @@
 # script that sets up your client SSH configuration file so that you can
 # connect to a server without typing a password.
-file { '~/.ssh/config':
-  ensure  => create,
-  content => 'Host *
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no',
+exec {'ssh_config':
+    path    => '/usr/bin/',
+    command => 'echo "IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> /etc/ssh/ssh_config'
 }
