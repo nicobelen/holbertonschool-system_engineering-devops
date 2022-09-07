4871a9e8cdcf44b06ac73c7e25356160a0434557
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-07 13:19:58 -0300

    Corrections on 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,14 +12,15 @@ if __name__ == "__main__":
 
     req_user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                             .format(sys.argv[1]))
-    req_name = req_user.json().get('name')
+    req_name = req_user.json().get('username')
 
     req_todos = requests.get('https://jsonplaceholder.typicode.com/users/\
 {}/todos'.format(sys.argv[1]))
     req_tasks = req_todos.json()
 
-    with open (sys.argv[1] + '.csv', 'w') as f:
+    with open(sys.argv[1] + '.csv', 'w') as f:
         my_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
 
         for task in req_tasks:
-            my_writer.writerow([sys.argv[1], req_name, task.get('completed'), task.get('title')])
+            my_writer.writerow([sys.argv[1], req_name, task.get('completed'),
+                                task.get('title')])
