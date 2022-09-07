460d6b27724ce3af79b9f047e264924c40d65ec6
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-06 23:30:48 -0300

    Correction on 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
                             .format(sys.argv[1]))
     req_name = req_user.json().get('name')
 
-    req_todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/\
-                             todos'.format(sys.argv[1]))
+    req_todos = requests.get('https://jsonplaceholder.typicode.com/users/\
+{}/todos'.format(sys.argv[1]))
     req_tasks = req_todos.json()
 
     req_len_tasks = len(req_tasks)
@@ -26,7 +26,8 @@ if __name__ == "__main__":
         if task["completed"] is True:
             all_tasks.append(task.get('title'))
             count += 1
-    print("Employee {} is done with tasks({}/{}):".format(req_name, count, req_len_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(req_name, count,
+          req_len_tasks))
 
     for title in all_tasks:
         print("\t {}".format(title))
