ef1aa6f01ca5c1876f08776ac1042c89aec639cf
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-26 15:05:54 -0300

    Pushing 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,5 @@
-# A puppet script
+# Puppet script that Change the OS configuration so that it is possible
+# to login with the holberton user and open a file without any error message.
 exec {
     'sed -i "s/holberton/foo/" /etc/security/limits.conf':
     }
